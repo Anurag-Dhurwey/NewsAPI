dc6dcd7e297fb13f8cb836ca03ebc4665078f07f
Anurag dhurwey <anuragdhurwey9211@gmail.com>
2022-12-01 16:25:20 +0530

    Fetch API is used in this modification

--- src/Comps/News.js
+++ src/Comps/News.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export default class News extends Component {
   render() {
-    let {title,description,imgUrl,newsUrl}=this.props
+    let {title,description,imgUrl,newsUrl}=this.props;
+    let setImg="https://picsum.photos/450/201";
+    let setDescription="To read click on below button";
     return (
       <>
-         <div className="card" style={{width: "16rem"}}>
-          <img src={imgUrl} className="card-img-top" alt="..."/>
+         <div className="card my-3" style={{width: "16rem"}}>
+          <img src={!imgUrl?setImg:imgUrl} className="card-img-top" alt="..."/>
           <div className="card-body text-center">
-            <h5 className="card-title">{title}......</h5>
-            <p className="card-text">{description}..........</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{!description?setDescription:description}</p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more</a>
           </div>
        </div>
--- src/Comps/NewsComp.js
+++ src/Comps/NewsComp.js
@@ -4,77 +4,32 @@ import News from './News'
 export default class NewsComp extends Component {
 
   
-  articles= [
-    {
-      "source": {
-        "id": "google-news-ca",
-        "name": "Google News (Canada)"
-      },
-      "author": "Noor Javed",
-      "title": "Doug Ford’s plan to suddenly add this small town into the Greenbelt puzzles critics and residents",
-      "description": "As part of its plan to open 7,400 acres of the Greenbelt to development, the province pledged to add 9,400 acres — most of them in the town of Erin.",
-      "url": "https://www.thestar.com/news/gta/2022/11/29/doug-fords-plan-to-suddenly-add-this-small-town-into-the-greenbelt-puzzles-critics-and-residents.html",
-      "urlToImage": "https://images.thestar.com/NGRtL4u1xhDEUMl3jbtnmNZ5af0=/1200x800/smart/filters:cb(1669742226948):format(webp)/https://www.thestar.com/content/dam/thestar/news/gta/2022/11/29/doug-fords-plan-to-suddenly-add-this-small-town-into-the-greenbelt-puzzles-critics-and-residents/afw_greenbeltaddition03.jpg",
-      "publishedAt": "2022-11-29T10:13:37+00:00",
-      "content": "When the province announced earlier this month it was opening up 7,400 acres of the Greenbelt to expedite development because of the housing shortage, it also pledged to put 9,400 acres into the prot… [+6612 chars]"
-    },
-    {
-      "source": {
-        "id": "entertainment-weekly",
-        "name": "Entertainment Weekly"
-      },
-      "author": "Patrick Gomez",
-      "title": "<em>9-1-1</em> showrunner explains that 'important' death — and previews the second half of season 6",
-      "description": "Executive producer Kristen Reidel breaks down how Wendall's death, Buck's sperm donor developments, and more will impact the rest of the season.",
-      "url": "https://ew.com/tv/9-1-1-season-6-winter-finale-death-preview-interview/",
-      "urlToImage": "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&rect=0%2C49%2C2000%2C1049&poi=%5B1360%2C413%5D&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2022%2F11%2F28%2F911-112822_2.jpg",
-      "publishedAt": "2022-11-29T07:00:00Z",
-      "content": "9-1-1 showrunner and executive producer Kristen Reidel promised that Monday's midseason finale would be \"fraught,\" and the episode definitely delivered.\r\nWhile \"Red Flag\" was buoyed by the news that … [+4670 chars]"
-    },
-    {
-      "source": {
-        "id": "google-news-ca",
-        "name": "Google News (Canada)"
-      },
-      "author": null,
-      "title": "Ontario passes housing bill despite criticism from municipalities over funding",
-      "description": "The Ontario government has passed housing legislation that overrides some municipal zoning laws and eliminates some development fees in an effort to follow through on the province’s goal of building 1.5 million homes.",
-      "url": "https://toronto.ctvnews.ca/ontario-passes-housing-bill-despite-criticism-from-municipalities-over-funding-1.6171507",
-      "urlToImage": "https://www.ctvnews.ca/content/dam/ctvnews/en/images/2022/10/26/doug-ford-1-6126096-1666806124896.jpg",
-      "publishedAt": "2022-11-28T17:00:00+00:00",
-      "content": "The Ontario government has passed housing legislation that overrides some municipal zoning laws and eliminates some development fees in an effort to follow through on the provinces goal of building 1… [+5486 chars]"
-    },
-    {
-      "source": {
-        "id": "medical-news-today",
-        "name": "Medical News Today"
-      },
-      "author": "Corrie Pelc",
-      "title": "Dementia vaccines: What are they, and when could they become available?",
-      "description": "There are many experimental dementia vaccines currently under development, but how are they meant to work, exactly, and when might such a vaccine become available?",
-      "url": "http://www.medicalnewstoday.com/articles/dementia-vaccines-what-are-they-and-when-could-they-become-available",
-      "urlToImage": "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2022/11/Vaccines_dementia_GettyImages1277352629_Thumb.jpg",
-      "publishedAt": "2022-11-21T17:00:00Z",
-      "content": "Vaccines are arguably one of the greatest inventions of medical science of all time. Now, researchers are looking to take vaccine technology one step further to protect against neurodegenerative dise… [+9679 chars]"
-    }
-  ]
+
 
 constructor(){
   super();
   this.state={
-    articles: this.articles
+    articles:[]
   }
+}
+
+ async componentDidMount(){
+  let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2b8542c3f43e4009802f8aa7c7be93a4";
+  let data=await fetch(url);
+  let parsedata= await data.json();
+ 
+  this.setState({articles:parsedata.articles})
 }
 
 
   render() {
     return (
       <>
-      <div className='container pt-2'>
+      <div className='container my-2 '>
         <div className='row'>
           {this.state.articles.map((element)=>{
-           return <div className='col-md-3 m-0' key={element.url}>
-            <News  title={element.title.slice(0,40)} description={element.description.slice(0,70)} imgUrl={element.urlToImage} newsUrl={element.url}/>
+           return <div className='col-md-3 ' key={element.url}>
+            <News  title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,70):""} imgUrl={element.urlToImage} newsUrl={element.url}/>
             </div>     
           //  "https://picsum.photos/450/201"
           })}
